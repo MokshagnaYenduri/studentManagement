b3aa870036a84f11902028f0b4b833d4d998f248
Mokshagna <mokshagnayenduri@gmail.com>
2025-09-24 12:24:56 +0530

    update fetch one student in studentController

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -67,3 +67,16 @@ export const getAllStudents = async (req, res) => {
         res.status(500).json({message: 'Internal server error'});
     }
 }
+
+export const getStudent = async (req, res) => {
+    const {rollNumber} = req.params;
+    try {
+        const student = await Student.findOne({rollNumber});
+        if(!student) {
+            return res.status(404).json({message: 'Student not found'});
+        }
+        res.status(200).json(student);
+    } catch (error) {
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
