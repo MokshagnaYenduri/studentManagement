1dc8e4d58ca9517aec23360b7200060ea0058e6e
Mokshagna <mokshagnayenduri@gmail.com>
2025-09-24 12:09:33 +0530

    update student schema to add roll Numer

--- backend/models/student.js
+++ backend/models/student.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const studentSchema = new mongoose.Schema({
+    rollNumber: {type: String, required: true, unique: true},
     studentName: {type: String, required: true},
     course: {type: String, required: true},
     cgpa: {type: Number, required: true},
