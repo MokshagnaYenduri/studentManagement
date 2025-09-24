630b14f177871ec7b47f9972a0c1a20ffff4b277
Mokshagna <mokshagnayenduri@gmail.com>
2025-09-24 12:21:13 +0530

    update studnet conterller to edit records

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -24,3 +24,24 @@ export const createStudent = async (req, res) => {
         res.status(500).json({message: 'Internal server error'});
     }
 }
+
+export const updateStudent = async (req, res) => {
+    const {rollNumber} = req.params;
+    const {studentName, course, cgpa} = req.body;
+
+    try {
+        const student = await Student.findOne({rollNumber});
+        if(!student) {
+            return res.status(404).json({message: 'Student not found'});
+        }
+        student.name = studentName || student.studentName;
+        student.course = course || student.course;
+        student.cgpa = cgpa || student.cgpa;
+
+        await student.save();
+        res.status(200).json({message: 'Student updated successfully'});
+    }
+    catch (error) {
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
