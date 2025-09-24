384367f7f601b7b989bc9af8f5b081c3598bae56
Mokshagna <mokshagnayenduri@gmail.com>
2025-09-24 11:33:20 +0530

    create root for app.js

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:2000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Student Management System API');
+});
+
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+});
+
+app.listen(process.env.PORT || 5172, () => {
+    console.log(`Server is running on port ${process.env.PORT || 5172}`);
+});
